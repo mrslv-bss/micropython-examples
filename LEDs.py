dab21a8d8b817f3032e05d5b106b6ad7a660e3a5
mrslv-bss <bassmiroslav@gmail.com>
2022-01-30 21:25:24 +0200

    Update LEDs.py

--- LEDs.py
+++ LEDs.py
@@ -1,17 +1,17 @@
 from machine import Pin, ADC
-import utime
+from utime import sleep
 
 
 L1 = Pin(2, Pin.OUT)
 L2 = Pin(3, Pin.OUT)
 L3 = Pin(4, Pin.OUT)
 L4 = Pin(5, Pin.OUT)
-LEDS = [L1,L2,L3,L4]
+LEDS = [L1, L2, L3, L4]
 pot = ADC(Pin(28))  # Potentiometer
 
 
 while True:
     for x in LEDS:
         x.high()
-        utime.sleep(pot.read_u16()/32768)
+        sleep(pot.read_u16()/32768)
         x.low()
