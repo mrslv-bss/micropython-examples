02e91543ddb2f99bcd366a57b0b2f0b25176d79d
mrslv-bss <bassmiroslav@gmail.com>
2022-01-30 21:23:08 +0200

    Update Servo.py

--- Servo.py
+++ Servo.py
@@ -1,5 +1,5 @@
 from machine import Pin, PWM, ADC
-import utime
+from utime import sleep
 
 
 pot = ADC(Pin(28))  # Potentiometer
@@ -10,4 +10,4 @@ servo.freq(50)
 while True:
     value = int(1350 + (pot.read_u16()/9.57)) # scales 0-65535 to 1350-8200
     servo.duty_u16(value) # duty_u16 == 1350 - zero degrees, 8200 - 180 degrees
-    utime.sleep(0.02)
+    sleep(0.02)
