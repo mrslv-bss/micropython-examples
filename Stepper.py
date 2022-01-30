3ea641bf3bb2e6f10d57eb98757dca88fe4d0226
mrslv-bss <bassmiroslav@gmail.com>
2022-01-30 21:15:33 +0200

    Update Stepper.py

--- Stepper.py
+++ Stepper.py
@@ -6,12 +6,22 @@ pin_b = Pin(1, Pin.OUT)
 pin_c = Pin(2, Pin.OUT)
 pin_d = Pin(3, Pin.OUT)
 
+mode = 'acw'
 rotate = [[1, 0, 0, 1], [1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 1, 1]]
 
+
+def rotater(step, delay):
+    pin_a.value(step[0])
+    pin_b.value(step[1])
+    pin_c.value(step[2])
+    pin_d.value(step[3])
+    utime.sleep(delay)
+
+
 while True:
-    for step in rotate:
-        pin_a.value(step[0])
-        pin_b.value(step[1])
-        pin_c.value(step[2])
-        pin_d.value(step[3])
-        utime.sleep(0.1)
+    if mode == 'cw':
+        for step in rotate:
+            rotater(step, 0.02)
+    elif mode == 'acw':
+        for step in reversed(rotate):
+            rotater(step, 0.02)
